42a5b67dd2f787fbee55461bf97ec9a1d657dd64
Elias Gabriel <me@eliasfgabriel.com>
2023-03-04 21:49:08 -0500

    fix: gevent monkeypatch
    ci: ignore until pdm migration

--- foghorn/__init__.py
+++ foghorn/__init__.py
@@ -1,5 +1,6 @@
 import gevent
+from gevent import monkey
 
-gevent.monkey.patch_all()
+monkey.patch_all()
 gevent.config.loop = "libuv"
 gevent.config.resolver = ["dnspython", "ares", "thread", "block"]
